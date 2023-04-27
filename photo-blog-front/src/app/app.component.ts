de636b6934f540f108e1092f05f821170b9d01e5
Tomek Dworak <tomasch107@gmail.com>
2023-04-27 08:59:45 +0200

    -initial commit

---
+++ photo-blog-front/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'photo-blog-front';
+
+  blogPost$: Observable<any> | undefined;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.blogPost$ = this.http.get('http://localhost:1337/api/posts')
+  }
+}

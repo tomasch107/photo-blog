b5e1392e5d2e6d6e497855901c57f120d4b5b462
Tomek Dworak <tomasch107@gmail.com>
2023-05-07 13:18:13 +0200

    -add some components

--- photo-blog-front/src/app/app.module.ts
+++ photo-blog-front/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ photo-blog-front/src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  protected readonly faBars = faBars;
+
+  mobileView = false;
+
+
+}

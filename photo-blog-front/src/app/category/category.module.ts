c86206df171c80d79e4b274df54141c80fa7cdb3
Tomek Dworak <tomasch107@gmail.com>
2023-10-03 22:44:36 +0200

    remove angular material

--- photo-blog-front/src/app/category/category.module.ts
+++ photo-blog-front/src/app/category/category.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './category/category.component';
 import {CategoryRoutingModule} from "./category-routing.module";
-import {MatCardModule} from "@angular/material/card";
 
 
 
@@ -13,7 +12,6 @@ import {MatCardModule} from "@angular/material/card";
   imports: [
     CommonModule,
     CategoryRoutingModule,
-    MatCardModule
   ]
 })
 export class CategoryModule { }
